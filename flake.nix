09dd8effac67eceafb9368ce4e924bd29767b1d4
sai <saitheninja@gmail.com>
2024-07-13 00:49:26 +0200

    explain nix flake

--- flake.nix
+++ flake.nix
@@ -6,17 +6,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/655a58a72a6601292512670343087c2d75d859c1";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 
-    in {
-      devShells.${system}.default = pkgs.mkShell { 
-        packages = with pkgs; [ 
-          zig
-        ]; 
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # build tools
+        # executed at build time, on buildPlatform
+        nativeBuildInputs = with pkgs; [ zig ];
 
+        # dependencies
+        # executed at run time, on hostPlatform
+        #buildInputs = [ ];
+
+        # `packages` is just combined with `nativeBuildInputs`
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/mkshell/default.nix#L41
+
+        # AN_ENVIRONMENT_VARIABLE = "something";
+
+        # execute when entering the shell environment with `nix develop`
         shellHook = ''
           zig version
         '';
